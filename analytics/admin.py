5eb7c2ddf96926b4321cbc1f56e8ae86e9d54ee6
Aman-apt <amanyadav665078@gmail.com>
2025-09-19 13:48:30 +0530

    Added the Amdmins for the core and analytics

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,7 +1,54 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import Session, Hit
 
 
-admin.site.register(Session)
-admin.site.register(Hit)
+class HitInline(admin.TabularInline):
+    model = Hit
+    fk_name = "session"
+    extra = 0
+
+
+class SessionAdmin(admin.ModelAdmin):
+    list_display = (
+        "uuid",
+        "service",
+        "start_time",
+        "last_seen",
+        "identifier",
+        "ip",
+        "asn",
+        "country",
+    )
+    list_display_links = ("uuid",)
+    search_fields = (
+        "ip",
+        "user_agent",
+        "device",
+        "device_type",
+        "identifier",
+        "asn",
+        "time_zone",
+    )
+    list_filter = ("device_type",)
+    inlines = [HitInline]
+
+
+admin.site.register(Session, SessionAdmin)
+
+
+class HitAdmin(admin.ModelAdmin):
+    list_display = (
+        "session",
+        "initial",
+        "start_time",
+        "heartbeats",
+        "tracker",
+        "load_time",
+        "location",
+    )
+    list_display_links = ("session",)
+    search_fields = ("initial", "tracker", "location", "referrer")
+    list_filter = ("initial", "tracker")
+
+
+admin.site.register(Hit, HitAdmin)
--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import Service
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import Service, User
 
-admin.site.register(Service)
+
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ("Extra fields", {"fields": ("api_token",)}),
+    )
+
+
+admin.site.register(User, UserAdmin)
+
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("name", "link", "owner", "status")
+    list_display_links = ("name",)
+    list_filter = ("status",)
+    search_fields = ("name", "link", "owner")
+
+
+admin.site.register(Service, ServiceAdmin)
